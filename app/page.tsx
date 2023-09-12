2cc1a9b6e2beacb3014ce285c078c7a82150258b
Lucas Moraes <lucasmo066@gmail.com>
2023-09-11 20:16:32 -0400

    add hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <Navbar />
+      <Hero />
     </main>
   )
 }
