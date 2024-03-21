a53c2a3323ddf31953bd34a9a2aa948c03357f08
Lucas Moraes <lucasmo066@gmail.com>
2024-03-21 17:33:00 -0400

    FCP fix

--- src/components/pages/home/hero/Hero.tsx
+++ src/components/pages/home/hero/Hero.tsx
@@ -4,6 +4,7 @@ import React, { useEffect } from "react";
 import { motion, useAnimation } from "framer-motion";
 import Image from "next/image";
 import AccentButton from "../../../shared/buttons/AccentButton";
+import logo from '/public/2.png'; // Make sure this path is correct. It might need adjustment based on your project structure.
 
 const Hero = () => {
   const headerText =
@@ -14,31 +15,17 @@ const Hero = () => {
   const controls = useAnimation();
 
   useEffect(() => {
-    // Sequence the animations one after the other
-    const sequenceAnimations = async () => {
-      await controls.start("visible");
-      await controls.start("visible");
-    };
-
-    sequenceAnimations();
+    // Start animations after a delay ensuring the first contentful paint occurs
+    controls.start("visible");
   }, [controls]);
 
   return (
-    <div
-      className="hero min-h-screen"
-      style={{ backgroundImage: "url(/hero.webP)" }}
-    >
+    <div className="hero min-h-screen" style={{ backgroundImage: "url(/hero.webP)" }}>
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md mx-auto">
-          {/* Logo Image Wrapper */}
           <div className="mb-6 hidden md:block" style={{ transform: 'translateX(27%)' }}>
-            <Image
-              src="/2.png"
-              width={200}
-              height={200}
-              alt="logo"
-            />
+            <Image src={logo} width={200} height={200} alt="logo" priority />
           </div>
           <motion.h1
             initial="hidden"
@@ -48,22 +35,15 @@ const Hero = () => {
                 opacity: 1,
                 transition: {
                   duration: 0.7,
-                  delay: 0.2,
-                  staggerChildren: 0.04,
+                  delay: 1, // Delay animations to ensure they start after initial content is painted
+                  staggerChildren: 0.05,
                 },
               },
               hidden: { opacity: 0 },
             }}
             className="mb-5 text-5xl font-bold text-secondary typing-text"
           >
-            {headerText.split("").map((char, index) => (
-              <motion.span
-                key={index}
-                variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }}
-              >
-                {char}
-              </motion.span>
-            ))}
+            {headerText}
           </motion.h1>
           <motion.p
             initial="hidden"
@@ -72,8 +52,8 @@ const Hero = () => {
               visible: {
                 opacity: 1,
                 transition: {
-                  duration: 1,
-                  delay: 0.5,
+                  duration: 0.7,
+                  delay: 1.2, // Further delay for subsequent content
                 },
               },
               hidden: { opacity: 0 },
@@ -89,8 +69,8 @@ const Hero = () => {
               visible: {
                 opacity: 1,
                 transition: {
-                  duration: 1,
-                  delay: 0.5,
+                  duration: 0.7,
+                  delay: 1.4, // Adjust delays as needed
                 },
               },
               hidden: { opacity: 0 },
