bb92ae270ce7f36a7d2c28f2ee08696d3e090a97
Lucas Moraes <lucasmo066@gmail.com>
2024-03-21 19:23:42 -0400

    fixed height of imageCarousel on CaPage

--- src/components/pages/cars/car/ImageCarousel.tsx
+++ src/components/pages/cars/car/ImageCarousel.tsx
@@ -7,21 +7,24 @@ interface ImageCarouselProps {
 
 const ImageCarousel: React.FC<ImageCarouselProps> = ({ images }) => {
   return (
-    <div className="carousel w-full">
+    <div className="carousel w-full overflow-hidden"> {/* Ensure the carousel container has overflow hidden */}
       {images.map((image, index) => (
         <div
           key={index}
           id={`slide${index + 1}`}
-          className="carousel-item relative w-full"
+          className="carousel-item relative w-full h-[500px] flex justify-center items-center" // Set a fixed height for each carousel item
         >
-          <Image
-            src={image}
-            alt="Car Image"
-            width={500}
-            height={200}
-            className="w-full"
-          />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          {/* Using the Image component with layout fill and objectFit to contain */}
+          <div className="relative w-full h-full">
+            <Image
+              src={image}
+              alt={`Car Image ${index + 1}`}
+              layout="fill"
+              objectFit="contain" // This ensures the image will scale down to fit within the container without distorting aspect ratio
+              className="w-full h-full"
+            />
+          </div>
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2 z-10">
             <a
               href={`#slide${index === 0 ? images.length : index}`}
               className="btn btn-circle"
